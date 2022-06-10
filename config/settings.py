bf5852d14159ce199be9789232a2bac2c8c392f1
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2022-06-10 17:12:06 +0430

    add static files

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 # for environment variables
 env = Env()
@@ -145,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
