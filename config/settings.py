c798c5206a243920f7b310fbe0ca4f24a7ab65ed
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2022-06-12 18:30:41 +0430

    add cart app

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'products.apps.ProductsConfig',
+    'cart.apps.CartConfig',
 
     # third party apps
     'rosetta',
