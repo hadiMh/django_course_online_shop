a574890be8fef3db24a715a7f44b0c4958a9f92b
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2022-06-15 10:00:01 +0430

    add short_description field to product model

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ from ckeditor.fields import RichTextField
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = RichTextField()
+    short_description = models.TextField(blank=True)
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
     image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
